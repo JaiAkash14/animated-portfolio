479645e7a2a8adb11f80a60a957285a6cba3c985
JaiAkash14 <jaiakashmurugan0@gmail.com>
2024-01-24 01:30:29 +0530

    parallax done

--- src/Componets/parallax/Parallax.jsx
+++ src/Componets/parallax/Parallax.jsx
@@ -18,7 +18,7 @@ export const Parallax = ({ type }) => {
         style={{ background: type === "services" ? "linear-gradient(180deg,#111132,#0c0c1d)" : "linear-gradient(180deg,#111132,#505064)" }}>
             <motion.h1 style={{ y: yText }} >{type === "Services" ? "What We Do?" : "What We Did?"}</motion.h1>
             <motion.div className="mountains"></motion.div>
-            <motion.div style={{ y: yBg }}className="planets"></motion.div>
+            <motion.div className="planets" style={{ y: yBg ,backgroundImage:`url(${type==="services"?"/planets.png":"/sun.png"})`}}></motion.div>
             <motion.div  style={{ x: yBg }}className="stars"></motion.div>
         </div>
     )
